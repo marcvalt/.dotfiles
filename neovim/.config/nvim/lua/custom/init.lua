9cb0a8f43c4f52ae6714b211401415acef31175c
marcvalt <themarcfm@gmail.com>
2022-11-29 12:46:32 +0100

    feat: yup yep

--- neovim/.config/nvim/lua/custom/init.lua
+++ neovim/.config/nvim/lua/custom/init.lua
@@ -1,5 +1,9 @@
 local autocmd = vim.api.nvim_create_autocmd
 
+local opt = vim.opt
+
+opt.relativenumber  = true
+
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
 --   pattern = "*",
